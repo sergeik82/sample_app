9d1410fa68e0d032bcc18b90937f81c659d98917
Sergei Kaganovsky <sergei@stashrewards.com>
2012-05-16 10:57:31 -0700

    User model validations

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -14,8 +14,8 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      #response.should have_selector("title",
-      #  :content => "Ruby on Rails Tutorials Sample App | Home")
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | Home")
 
 
     end
@@ -30,12 +30,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | Contact")
+    end
   end
   
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => "#{@base_title} | About")
     end
   end
 
