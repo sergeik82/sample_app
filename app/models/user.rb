8d67a6d87c270d6f357f43679f7354b7eb8c81bb
Sergei Kaganovsky <sergei@stashrewards.com>
2012-05-18 15:25:51 -0700

    Added a Signup form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#
 
 class User < ActiveRecord::Base
   attr_accessor   :password
@@ -47,16 +59,4 @@ class User < ActiveRecord::Base
     end
 end
 
-# == Schema Information
-#
-# Table name: users
-#
-#  id                 :integer         not null, primary key
-#  name               :string(255)
-#  email              :string(255)
-#  created_at         :datetime
-#  updated_at         :datetime
-#  encrypted_password :string(255)
-#  salt               :string(255)
-#
 
