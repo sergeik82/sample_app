c56972451f4627a076d1b2f301f70986da5fc64e
Sergei Kaganovsky <sergei@stashrewards.com>
2012-05-21 12:27:08 -0700

    Competed signin/out.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # Handle a successful save.
+      sign_in @user
       redirect_to @user, :flash => { :success => "Welcome to the Sample App!"}
     else
       @title = "Sign Up"
