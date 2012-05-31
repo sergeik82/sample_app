9c5a0bdd4aa6bd0591e24efa45411ea17dab97f6
Sergei Kaganovsky <sergei@stashrewards.com>
2012-05-30 19:28:44 -0700

    Showing microposts.

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -18,8 +18,7 @@ class Micropost < ActiveRecord::Base
   
   validates :content, :presence => true,
                       :length => { :maximum => 140 }
-                  
-  
+
   default_scope :order => 'microposts.created_at DESC'
 end
 
